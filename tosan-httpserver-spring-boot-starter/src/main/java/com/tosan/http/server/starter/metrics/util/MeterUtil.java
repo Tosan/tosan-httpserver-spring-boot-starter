4c569c3e0e4caeae7f89fbe63b548a04e9c77181
Shahryar safizade <78639744+shahryarsz@users.noreply.github.com>
2024-02-21 18:13:33 +0330

    [feature]adding fixed gauge meter and updating counter meter with amount

--- tosan-httpserver-spring-boot-starter/src/main/java/com/tosan/http/server/starter/metrics/util/MeterUtil.java
+++ tosan-httpserver-spring-boot-starter/src/main/java/com/tosan/http/server/starter/metrics/util/MeterUtil.java
@@ -38,6 +38,16 @@ public class MeterUtil {
         return meters.get(key);
     }
 
+    public Meter registerFixedInitGaugeMeter(String meterName, String description, Tags tags, Long value) {
+        String key = createKey(meterName, tags);
+        if (!meters.containsKey(key)) {
+            synchronized (meters) {
+                meters.computeIfAbsent(key, ignore -> registerFixedGaugeMeter(meterName, description, tags, value));
+            }
+        }
+        return meters.get(key);
+    }
+
     public void updateTimerMeter(String metricName, Tags tags, long duration) {
         String key = createKey(metricName, tags);
         if (!meters.isEmpty() && meters.get(key) != null) {
@@ -54,12 +64,19 @@ public class MeterUtil {
         }
     }
 
+    public void updateCounterMeterByFixedAmount(String metricName, Tags tags, double amount) {
+        String key = createKey(metricName, tags);
+        if (!meters.isEmpty() && meters.get(key) != null) {
+            Meter meter = meters.get(key);
+            ((Counter) meter).increment(amount);
+        }
+    }
+
     public void updateGaugeMeterByIncrementing(String metricName, Tags tags) {
         String key = createKey(metricName, tags);
         if (!meters.isEmpty() && gaugeMeters.get(key) != null) {
             GaugeValue gaugeValue = gaugeMeters.get(createKey(metricName, tags));
             gaugeValue.increment();
-            gaugeMeters.put(createKey(metricName, tags), gaugeValue);
         }
     }
 
@@ -68,7 +85,6 @@ public class MeterUtil {
         if (!gaugeMeters.isEmpty() && gaugeMeters.get(key) != null) {
             GaugeValue gaugeValue = gaugeMeters.get(createKey(metricName, tags));
             gaugeValue.decrement();
-            gaugeMeters.put(createKey(metricName, tags), gaugeValue);
         }
     }
 
@@ -129,20 +145,35 @@ public class MeterUtil {
         }
     }
 
-    private Gauge registerGaugeMeter(String metricName, String desctiption, Tags tags) {
+    private Gauge registerGaugeMeter(String metricName, String description, Tags tags) {
         GaugeValue gaugeValue = new GaugeValue();
         Gauge gauge;
         if (tags == null) {
             gauge = Gauge.builder(metricName, gaugeValue::getValue)
-                    .description(desctiption)
+                    .description(description)
                     .register(meterRegistry);
         } else {
             gauge = Gauge.builder(metricName, gaugeValue::getValue)
-                    .description(desctiption)
+                    .description(description)
                     .tags(tags)
                     .register(meterRegistry);
         }
         gaugeMeters.put(createKey(metricName, tags), gaugeValue);
+        return gauge;
+    }
+
+    private Gauge registerFixedGaugeMeter(String metricName, String description, Tags tags, Long value) {
+        Gauge gauge;
+        if (tags == null) {
+            gauge = Gauge.builder(metricName, value::longValue)
+                    .description(description)
+                    .register(meterRegistry);
+        } else {
+            gauge = Gauge.builder(metricName, value::longValue)
+                    .description(description)
+                    .tags(tags)
+                    .register(meterRegistry);
+        }
         return gauge;
     }
 }
